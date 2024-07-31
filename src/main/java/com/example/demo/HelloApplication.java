ff4f5d6619d23cad343821b6d1edae619fc13be6
Sunichii <sunichishrestha@gmail.com>
2024-07-31 16:29:37 +0545

    started java programming

---
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+public class HelloApplication{
+
+    public static void main(String[] args) {
+      System.out.println("Hello this it the first learning step of java");
+      System.out.println("Hello this is the 2nd step of learning jaava");
+    }
+}
